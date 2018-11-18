13dd101b62387974fc3f2cdb7a1a85cdd614de06
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2018-11-18 12:23:40 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+let server = app.listen(3000, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log(server.address());
+    console.log('Example app listening at http://%s:%s', host, port)
+});
